f0ba26603d0bb4e63f3df2b521dde20fe6ca827e
Bob Ralian <bobralian@jill-ralians-computer.local>
2011-01-14 02:46:40 -0600

    added code coverage tool

--- runtests.sh
+++ runtests.sh
@@ -4,4 +4,6 @@ if [[ "$OSTYPE" =~ darwin.+ ]]
 then export PYTHONPATH=/Applications/GoogleAppEngineLauncher.app/Contents/Resources/GoogleAppEngine-default.bundle/Contents/Resources/google_appengine:$PYTHONPATH
 fi
 
-python runtests.py
+coverage run runtests.py
+coverage report
+coverage html
